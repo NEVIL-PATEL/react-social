8a44ecf0e900b6bc79d4f668cb61fc4421a5016a
Nevil Patel <93375041+NEVIL-PATEL@users.noreply.github.com>
2022-11-29 20:41:27 +0530

    profile

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,21 @@
+import './login.css'
+
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='login'>
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className="loginLogo">Foriox</h3>
+                <span className="loginDesc">Connect with friends and the world around you on Foriox.</span>
+            </div>
+            <div className="loginRight">
+                <div className="loginBox">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
